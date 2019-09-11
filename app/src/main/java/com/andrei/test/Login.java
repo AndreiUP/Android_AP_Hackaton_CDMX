e67cd8daca72d52822c3634ae8710d298b6fd482
Manuel  Cervantes <manu@MacBook-Pro-de-Manuel.local>
2019-09-10 22:47:14 -0500

    menu lateral

--- app/src/main/java/com/andrei/test/Login.java
+++ app/src/main/java/com/andrei/test/Login.java
@@ -1,6 +1,10 @@
  package com.andrei.test;
 
+ import android.content.Intent;
  import android.os.Bundle;
+ import android.view.View;
+ import android.widget.Button;
+ import android.widget.EditText;
 
  import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,25 @@
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.login);
+
+
+         Button login = findViewById(R.id.signin);
+         Button registro = findViewById(R.id.register);
+         login.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(Login.this, HomeActivity.class);
+                  startActivity(intent);
+             }
+         });
+
+         registro.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(Login.this, RegisterActivity.class);
+                 startActivity(intent);
+             }
+         });
      }
+
  }
